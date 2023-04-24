007ed4e8caabb4162fb91bb3da915ad5f82de1f9
IrinaChoulgan <irina_choulgan@icloud.com>
2023-04-24 14:43:52 +0200

    add header component with style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logo from '../../assets/cat.png';
+
+const LogoSite = styled.a`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 1.67;
+  letter-spacing: 0.5px;
+  color: #f07f2e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoImg = styled.img`
+  width: 21px;
+  height: 21px;
+  margin-right: 9px;
+`;
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
+`;
+
+const HeaderNavigation = styled.nav`
+  display: flex;
+  gap: 40px;
+`;
+const NavigationLink = styled.a`
+  color: #2c2c2c;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 1.5;
+  letter-spacing: 0.5px;
+`;
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <HeaderWrapper>
+      <LogoSite href="/">
+        <LogoImg src={Logo} alt="logo" />
+        Котодім
+      </LogoSite>
+      <HeaderNavigation>
+        <NavigationLink href="">Обрати котика</NavigationLink>
+        <NavigationLink href="">Задонатити котикам</NavigationLink>
+        <NavigationLink href="">Контакти</NavigationLink>
+      </HeaderNavigation>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
