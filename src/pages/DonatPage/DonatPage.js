4e53bc58f1d6f54387a272ce190ea9cd027b00db
IrinaChoulgan <irina_choulgan@icloud.com>
2023-05-10 16:23:51 +0200

    add donat page

--- src/pages/DonatPage/DonatPage.js
+++ src/pages/DonatPage/DonatPage.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Header, Footer, Container } from '../../components';
+import QR from '../../assets/QR.png';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+`;
 
 const DonatPage = () => {
-  return <div>Donat</div>;
+  return (
+    <div>
+      <Header />
+      <Container>
+        <StyledWrapper>
+          <h2>
+            Тут може бути QR-код з реквізитами для підтримки котиків, а поки що туди "вшите" зображення
+            котика)
+          </h2>
+          <img src={QR} alt="QR" width={'400px'} />
+        </StyledWrapper>
+      </Container>
+      <Footer />
+    </div>
+  );
 };
 
 export default DonatPage;
